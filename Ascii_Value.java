ac84229dea38cc440addf37517de5689021abc2f
Prashila-Mali <116636396+Prashila-Mali@users.noreply.github.com>
2022-11-19 23:01:09 +0530

    Day 3

---
+++ Ascii_Value.java
@@ -0,0 +1,14 @@
+// DAY 3
+import java.util.*;
+class Ascii_Value
+{
+	public static void main(String args[])
+	{
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter a Character: ");
+		char ch=sc.next().charAt(0);
+
+		int a = ch;
+		System.out.println("Tha ASCII value of :"+a);
+	}
+}
