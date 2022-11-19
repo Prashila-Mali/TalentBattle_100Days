9b791441efe60388cd3e8210a1a6527b3947ef26
Prashila-Mali <116636396+Prashila-Mali@users.noreply.github.com>
2022-11-19 23:00:39 +0530

    Day 2

---
+++ CharAlpha.java
@@ -0,0 +1,22 @@
+//DAY 2 :
+
+import java.util.*;
+ class CharAlpha {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a Character:");
+        char ch=sc.next().charAt(0);
+
+        if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z'))
+        {
+            System.out.println(ch + " is Alphabet");
+        }
+        
+        else
+        {
+        	 System.out.println(ch + "Not Alphabet");
+        }
+    }
+}
