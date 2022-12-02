3436326a8be205184f1940fc7296a8895bef2497
Prashila-Mali <116636396+Prashila-Mali@users.noreply.github.com>
2022-12-02 19:20:15 +0530

    Day 17

---
+++ Factor_Num.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Factor_Num
+{
+    public static void main(String args [])
+    {
+    	Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number :");
+    	int n = sc.nextInt();
+      
+        for  (int i = 1; i <= n; i++)
+         {
+             if (n % i == 0) 
+             {
+                System.out.print(i + ",");
+             }
+             
+        }
+
+        
+    }
+
+}
