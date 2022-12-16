173422cee29ca064505ab43e43d5ba6d4237302d
Prashila-Mali <116636396+Prashila-Mali@users.noreply.github.com>
2022-12-16 22:56:30 +0530

    Day 25

---
+++ AreaOfCircle.java
@@ -0,0 +1,14 @@
+//Day 25
+
+import java.util.*;
+class AreaOfCircle
+{
+	public static void main(String args[])
+	{
+         Scanner sc = new Scanner(System.in);
+         System.out.println("Enter the radius :");
+         float r = sc.nextFloat();
+        // float area = (3.14*r*r);
+         System.out.println("Area of Circle :"+ (3.14*r*r));
+	}
+}
