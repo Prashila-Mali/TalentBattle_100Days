59a12280557157f9803ba36c22c2e23979f4f7f5
Prashila-Mali <116636396+Prashila-Mali@users.noreply.github.com>
2022-12-17 15:05:51 +0530

    Day 29

---
+++ ConcatString.java
@@ -0,0 +1,19 @@
+//Day 29
+
+import java.util.*;
+class ConcatString
+{
+	public static void main(String args[])
+	{
+	     Scanner sc = new Scanner(System.in);
+	     System.out.println("Enter a First String :");
+	     String str1 = sc.next();
+
+	     System.out.println("Enter a Second String :");
+	     String str2 = sc.next();
+
+	     System.out.println((str1+str2));
+
+
+	}
+}
