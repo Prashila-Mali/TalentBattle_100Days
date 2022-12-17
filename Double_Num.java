0a34b8c26497af4cdedce384fd3200bf242f1d81
Prashila-Mali <116636396+Prashila-Mali@users.noreply.github.com>
2022-12-17 15:03:47 +0530

    Day 27

---
+++ Double_Num.java
@@ -0,0 +1,19 @@
+//Day 27
+
+import java.util.*;
+class Double_Num
+{
+	
+	public static void main(String args[])
+	{
+		int i;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Number :");
+		int num=sc.nextInt();
+		for(i=num;i<num+num;i++)
+		{
+			continue; 
+		}
+		System.out.println(i);
+	}
+}
