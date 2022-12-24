1c9cafb62ad2f13f1a5f2da344f7e2be5aa5cddb
Prashila-Mali <116636396+Prashila-Mali@users.noreply.github.com>
2022-12-24 22:20:42 +0530

    Day 37

---
+++ Cal_Frequency.java
@@ -0,0 +1,33 @@
+//Day 37
+
+import java.util.*;
+ class Cal_Frequency
+ {
+    public static void main(String[] args) 
+    {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        int[] frequency = new int[str.length()];
+        int i, j;
+        char strnew[] = str.toCharArray();
+        for(i = 0; i <str.length(); i++) 
+        {
+            frequency[i] = 1;
+            for(j = i+1; j <str.length(); j++) 
+            {
+                if(strnew[i] == strnew[j]) 
+                {
+                    frequency[i]++;
+                    strnew[j] = '0';
+                }
+            }
+        }
+        System.out.println("Frequencies: ");
+        for(i = 0; i <frequency.length; i++) 
+        {
+            if(strnew[i] != ' ' && strnew[i] != '0')
+            System.out.println(strnew[i] + " - " + frequency[i]);
+        } 
+    }
+ }
