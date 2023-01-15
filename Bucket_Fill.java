8d8ce594da54e3f7cf9fa33ef0b7a672c37d79e9
Prashila-Mali <116636396+Prashila-Mali@users.noreply.github.com>
2023-01-15 13:35:26 +0530

    Day 58

---
+++ Bucket_Fill.java
@@ -0,0 +1,19 @@
+//Day 58
+
+import java.util.*;
+import static java.lang.System.out;
+class Bucket_Fill
+{
+   public static void main(String[] args) 
+   {
+   		Scanner sc = new Scanner(System.in);
+   		out.println("Enter the capacity of k liters:");
+   		int k = sc.nextInt();
+
+   		out.println("Enter the capacity of x liters:");
+   		int x = sc.nextInt();
+
+   		out.println(k-x);
+
+   }	
+}
